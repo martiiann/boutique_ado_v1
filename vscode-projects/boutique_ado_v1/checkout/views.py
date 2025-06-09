ad62105bcdb4b6c49fb38b5fe6407fe26d65a44e
martiiann <martiian03817@icloud.com>
2025-06-09 23:40:02 +0100

    Added stripe elements

--- vscode-projects/boutique_ado_v1/checkout/views.py
+++ vscode-projects/boutique_ado_v1/checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51RYEAnB6KDiuq9RCOgNdfzKTTv31Ajh8PPntK5ScnmE6RVo3Tzim8OvlOdyNFTGQnNfWOXcdBGYPsC90xaU7a8iF00Dx45yGyl',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
